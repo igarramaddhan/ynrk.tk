b745827036a2bfc674284bfef8bc8cf4f7f5ffd7
Igar Ramaddhan <igarramaddhan@gmail.com>
2019-02-08 20:04:31 +0700

    change config name

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `ynrk.tk`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#f2f2f2`,
