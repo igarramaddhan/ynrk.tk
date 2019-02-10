bb59c95f4e7fc735e6cd433ffca9884c82398f8d
Igar Ramaddhan <igarramaddhan@gmail.com>
2019-02-10 08:19:21 +0700

    change manifest theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#f2f2f2`,
-        theme_color: `#f2f2f2`,
+        theme_color: `#ff9900`,
         display: `minimal-ui`,
         icon: `src/images/YLogo.png` // This path is relative to the root of the site.
       }
